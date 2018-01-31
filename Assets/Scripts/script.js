67bc3bb0e626a1c9d9b460227ca4f80247a18b2f
catalyticstorm <catalyticstorm@roadrunner.com>
2018-01-31 09:30:22 -0500

    Put Firebrand in Jscript, Updated API key

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,1 +1,28 @@
 // JavaScript Document
+
+var movieData;
+var movieChosen;
+var eventData;
+var eventChosen;
+
+//Ticketmaster API
+var ticketmasterURL = "";
+var ticketmasterKey = "VFqKbEqAQRwPLtAKW0UynnLWlq3YTFkY";
+var ticketmasterSecret = "X0iHZkGjS3HUf3Y1";
+
+//Google Maps API
+var gMapsDirKey = "AIzaSyB40cmHuwMhUBf1kD6wbqr9lXL4C6R4a5M";
+
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDNzLkz1J-tdFkbsERtH1DGNOJy-QRDXMY",
+	authDomain: "dinnermovieapi.firebaseapp.com",
+	databaseURL: "https://dinnermovieapi.firebaseio.com",
+	projectId: "dinnermovieapi",
+	storageBucket: "",
+	messagingSenderId: "658504571017"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
